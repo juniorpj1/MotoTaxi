0cc8033c550bece79d6c2833ecfbaae907b2a011
brunota10 <bruno_ta10@hotmail.com>
2015-10-30 12:05:44 -0200

    alterações nos casos de usos 7 e 8.

--- src/aplicacao/Principal.java
+++ src/aplicacao/Principal.java
@@ -1,6 +1,7 @@
 package aplicacao;
 
 import java.math.BigDecimal;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -12,12 +13,14 @@ import dominio.Bandeirada;
 import dominio.Cliente;
 import dominio.Empresa;
 import dominio.MotoTaxista;
+import dominio.Chamado;
 import servico.AvaliacaoServico;
 import servico.BandeiradaServico;
 import servico.ChamadoServico;
 import servico.ClienteServico;
 import servico.EmpresaServico;
 import servico.MotoTaxistaServico;
+import servico.ServicoException;
 import servico.ServicoFactory;
 import servico.impl.EmpresaServicoImpl;
 
@@ -99,7 +102,151 @@ public class Principal {
 				System.out.println("Caso de uso ainda não implementado - Ana");
 				break;
 			case 7:
-				System.out.println("Caso de uso ainda não implementado - Ana");
+				String endOrigem, endDestino;
+				Double distancia;
+				int cliente,mototaxista, codigo1;
+				Bandeirada b;
+				do{
+					System.out.println("\nEscolha uma das opções do menu abaixo:");
+					System.out.println("1 - Inserir");
+					System.out.println("2 - Alterar");
+					System.out.println("3 - Pesquisar");
+					System.out.println("4 - Excluir");
+					System.out.println("5 - Sair");
+					opcao = Integer.parseInt(sc.nextLine());
+
+					switch (opcao) {
+					case 1:	
+						System.out.println("Digite o endereço de origem: ");
+						endOrigem = sc.nextLine();
+						System.out.println("Digite o endereço de destino: ");
+						endDestino = sc.nextLine();
+						System.out.println("Informe a distância aproximada em KM: ");
+						distancia = Double.parseDouble(sc.nextLine());
+						System.out.println("Informe seu código de cliente: ");
+						cliente = Integer.parseInt(sc.nextLine());
+						
+						Cliente cli = clienteServico.buscar(cliente);
+						Date agora = new java.util.Date(); 
+						
+						if(cli == null){
+							System.out.println("Erro: Código do cliente não encontrado. Forneça um código válido!!!");
+						}else{
+							System.out.println("Informe o código do mototaxista: ");
+							mototaxista= Integer.parseInt(sc.nextLine());
+							
+							MotoTaxista mot = motoTaxistaServico.buscar(mototaxista);
+							if(mot == null){
+								System.out.println("Erro: Código do mototaxista não encontrado. Forneça um código válido!!!");
+							}else{ 
+								if(agora.getHours() > 18){
+									b = bandeiradaServico.buscar(1);
+								}
+								else{
+									b = bandeiradaServico.buscar(2);
+								}
+								
+								Chamado cha = new Chamado(null,endOrigem,endDestino,agora,distancia,cli,mot,b);
+								try {
+									chamadoServico.inserirChamado(cha);
+									System.out.println("Chamado criado com sucesso!!!");
+								} catch (ServicoException e) {
+									System.out.println("Erro: Chamado não inserido. Verificar erro!!");
+								}
+							}
+						}
+						break;
+					case 2:		
+						System.out.println("Digite o código do chamado: ");
+						codigo1 = Integer.parseInt(sc.nextLine());
+						
+						Chamado c = chamadoServico.buscar(codigo1);
+						
+						if(c == null){
+							
+						}else{
+							System.out.println("Digite o endereço de origem: ");
+							endOrigem = sc.nextLine();
+							System.out.println("Digite o endereço de destino: ");
+							endDestino = sc.nextLine();
+							System.out.println("Informe a distância aproximada em KM: ");
+							distancia = Double.parseDouble(sc.nextLine());
+							System.out.println("Informe seu código de cliente: ");
+							cliente = Integer.parseInt(sc.nextLine());
+							
+							Cliente cli1 = clienteServico.buscar(cliente);
+							Date agora2 = new java.util.Date(); 
+							
+							if(cli1 == null){
+								System.out.println("Erro: Código do cliente não encontrado. Forneça um código válido!!!");
+							}else{
+								System.out.println("Informe o código do mototaxista: ");
+								mototaxista= Integer.parseInt(sc.nextLine());
+								
+								MotoTaxista mot = motoTaxistaServico.buscar(mototaxista);
+								if(mot == null){
+									System.out.println("Erro: Código do mototaxista não encontrado. Forneça um código válido!!!");
+								}else{ 
+									if(agora2.getHours() > 18){
+										b = bandeiradaServico.buscar(1);
+									}
+									else{
+										b = bandeiradaServico.buscar(2);
+									}
+									
+									Chamado cha = new Chamado(codigo1,endOrigem,endDestino,agora2,distancia,cli1,mot,b);
+									try {
+										chamadoServico.inserirChamado(cha);
+										System.out.println("Chamado alterado com sucesso!!!");
+									} catch (ServicoException e) {
+										System.out.println("Erro: Chamado não alterado. Verificar erro!!");
+									}
+								}
+							}
+						}				
+						break;
+					case 3:
+						System.out.println("Digite o código do chamado a ser pesquisado:");
+						codigo1 = Integer.parseInt(sc.nextLine());
+						
+						Chamado ch = chamadoServico.buscar(codigo1);
+						
+								if(ch == null){
+									System.out.println("Erro: Chamado não encontrada. Digite outro valor válido!!!");
+		
+								}else{
+									System.out.println("Código do chamado: " + ch.getCodChamado());
+									System.out.println("Endereço de Origem: " + ch.getOrigem());
+									System.out.println("Endereço de Destino: " + ch.getDestino());
+									System.out.println("Hora do Chamado: " + ch.getDataHora());
+									System.out.println("Distância Aproximada em KM: " + ch.getDistancia());
+									System.out.println("Nome do Cliente: " + ch.getCliente().getNome());
+									System.out.println("Nome do Mototaxista: " + ch.getMotoTaxista().getNome());
+									System.out.println("Valor Básico da Bandeirada: R$ " + ch.getBandeirada().getValorInicial());
+							}
+						break;
+					case 4:
+						System.out.println("Digite o código do chamado:");
+						codigo1 = Integer.parseInt(sc.nextLine());
+						
+						Chamado cha = chamadoServico.buscar(codigo1);
+						
+								if(cha == null){
+									System.out.println("Erro: Chamado não encontrado. Digite outro valor válido!!!");
+		
+								}else{
+									chamadoServico.excluir(cha);
+									System.out.println("Chamado excluído com sucesso!!!");
+							}
+						break;
+					case 5:
+						System.out.println("**** ALTERAÇÕES NOS CHAMADOS FINALIZADAS COM SUCESSO!!!****\n");
+						break;
+						
+					default:
+						System.out.println("Opção inválida!!!");
+					}
+				} while(opcao != 5);
 				break;
 			case 8:
 				BigDecimal valor6, valor7;
@@ -174,7 +321,7 @@ public class Principal {
 						Bandeirada bandeira = bandeiradaServico.buscar(codigo);
 						
 								if(bandeira == null){
-									System.out.println("Erro: Bandeirada não encontrada. Encontre outro valor!!!");
+									System.out.println("Erro: Bandeirada não encontrada. Digite outro valor válido!!!");
 		
 								}else{
 									bandeiradaServico.excluir(bandeira);
@@ -182,7 +329,7 @@ public class Principal {
 							}
 						break;
 					case 5:
-						System.out.println("Alterações nas bandeiradas finalizadas.\n");
+						System.out.println("**** ALTERAÇÕES NAS BANDEIRADAS FINALIZADAS COM SUCESSO!!!****\n");
 						break;
 						
 					default:
@@ -225,6 +372,7 @@ public class Principal {
 			}
 
 		} while (op != 14);
+		
 
 	}
 
